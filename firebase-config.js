04872fc2927705638c9fd14aefbc5975365d30d1
shidheswarnath-mandir-samiti-dotcom <shivratricomitteebishoultatuar@gmail.com>
2026-06-21 15:13:37 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// ===== FIREBASE CONFIGURATION =====
+// Firebase SDK ko CDN se load kar rahe hain (HTML files mein <script type="module"> use hoga)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJBHAyw1mJAzk_ZxtRkdhk4UjW65Zehac",
+  authDomain: "sidheswar-nath-mandir.firebaseapp.com",
+  projectId: "sidheswar-nath-mandir",
+  storageBucket: "sidheswar-nath-mandir.firebasestorage.app",
+  messagingSenderId: "881069210651",
+  appId: "1:881069210651:web:7afffc665f07eb8f1e4cbe"
+};
---
+++ sheet-config.js
@@ -0,0 +1,7 @@
+const SHEET_URLS = {
+  summary:    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTtDEIc3fq76YEp_eVN_BDqwOXPKAmtFyzTxbrTsB8fjjJIUOQzMejMmWmOk2RtfWc1SPbNmdtZnHvE/pub?gid=606641288&single=true&output=csv",
+  members:    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTtDEIc3fq76YEp_eVN_BDqwOXPKAmtFyzTxbrTsB8fjjJIUOQzMejMmWmOk2RtfWc1SPbNmdtZnHvE/pub?gid=0&single=true&output=csv",
+  market:     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTtDEIc3fq76YEp_eVN_BDqwOXPKAmtFyzTxbrTsB8fjjJIUOQzMejMmWmOk2RtfWc1SPbNmdtZnHvE/pub?gid=189899740&single=true&output=csv",
+  shubhKarya: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTtDEIc3fq76YEp_eVN_BDqwOXPKAmtFyzTxbrTsB8fjjJIUOQzMejMmWmOk2RtfWc1SPbNmdtZnHvE/pub?gid=610071063&single=true&output=csv",
+  expenses:   "https://docs.google.com/spreadsheets/d/e/2PACX-1vTtDEIc3fq76YEp_eVN_BDqwOXPKAmtFyzTxbrTsB8fjjJIUOQzMejMmWmOk2RtfWc1SPbNmdtZnHvE/pub?gid=421525165&single=true&output=csv"
+};
